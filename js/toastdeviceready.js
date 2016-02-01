e3979e0c3000ba4a34ad09de3c856d017fa10d19
unknown <olucero7884@2A304-C01.conestogac.on.ca>
2016-02-01 14:48:29 -0500

    url

---
+++ js/toastdeviceready.js
@@ -0,0 +1,8 @@
+var phonegapReady = function(){
+    new jQuery.nd2Toast({message:"we are in phonegap"});
+};
+
+jQuery(document).ready(function(){
+    document.addEventListener("deviceready",
+phonegaoReady, false);
+});
